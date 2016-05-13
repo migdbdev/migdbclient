040d79ec773654f3cff27e521bbea32d413e1219
Kanishka_Lahiru <lkanishka200@gmail.com>
2016-05-14 02:08:45 +0530

    Add Favicon
    Add favicon to MigDB application. Favicon is in under image package

--- src/org/migdb/migdbclient/main/MainApp.java
+++ src/org/migdb/migdbclient/main/MainApp.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -18,6 +19,7 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("MigDB");
+		this.primaryStage.getIcons().add(new Image("/org/migdb/migdbclient/images/MigDB.png"));
 		initRootLayout();
 	}
 
