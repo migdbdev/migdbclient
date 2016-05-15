d014430e9ac5a2a2e57dbceaa5f64de6e1dc4327
lakshankasun <lkmadhusanka@gmail.com>
2016-05-15 15:40:11 +0530

    added jackson library

---
+++ src/org/migdb/migdbclient/views/mongodatamanager/MongoDataManager.java
@@ -0,0 +1,16 @@
+package org.migdb.migdbclient.views.mongodatamanager;
+
+import javax.management.NotificationBroadcasterSupport;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MongoDataManager {
+	@FXML private Button btnTest;
+	
+	@FXML
+	public void display(){
+		System.out.println("jkdhfkjsdhgkjs jshvkjs ");
+	}
+	
+}
