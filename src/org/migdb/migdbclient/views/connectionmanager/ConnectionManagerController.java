e6df6544feb4d233100d6a286bf20098bfcffdfc
Kanishka_Lahiru <lkanishka200@gmail.com>
2016-05-17 23:47:09 +0530

    Add connection manager
    Add navigation between connection manager windows

--- src/org/migdb/migdbclient/views/connectionmanager/ConnectionManagerController.java
+++ src/org/migdb/migdbclient/views/connectionmanager/ConnectionManagerController.java
@@ -1,5 +1,68 @@
 package org.migdb.migdbclient.views.connectionmanager;
 
-public class ConnectionManagerController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.migdb.migdbclient.config.FxmlPath;
+import org.migdb.migdbclient.main.MainApp;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ConnectionManagerController implements Initializable {
+
+	private AnchorPane rootLayoutAnchorpane;
+	@FXML
+	private Label addConnectionLabel;
+
+	/**
+	 * Initialize method Called to initialize a controller after its root
+	 * element has been completely processed The location used to resolve
+	 * relative paths for the root object, or null if the location is not known
+	 * The resources used to localize the root object, or null if the root
+	 * object was not localized
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// Add connection label click event
+		addConnectionLabel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent mouseevent) {
+				createNewConnectionPopup();
+			}
+		});
+
+	}
+
+	/**
+	 * Create new connection manager pop up window method
+	 */
+	public void createNewConnectionPopup() {
+
+		try {
+			Stage newConnectionStage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource(FxmlPath.SETUPNEWDBCONNECTION.getPath()));
+			rootLayoutAnchorpane = loader.load();
+			newConnectionStage.setTitle("Create a new connection");
+			newConnectionStage.setAlwaysOnTop(true);
+			newConnectionStage.setResizable(false);
+			newConnectionStage.centerOnScreen();
+			Scene scene = new Scene(rootLayoutAnchorpane);
+			newConnectionStage.setScene(scene);
+			newConnectionStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
--- src/org/migdb/migdbclient/views/connectionmanager/SetupNewDBConnectionController.java
+++ src/org/migdb/migdbclient/views/connectionmanager/SetupNewDBConnectionController.java
@@ -1,5 +1,76 @@
 package org.migdb.migdbclient.views.connectionmanager;
 
-public class SetupNewDBConnectionController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.migdb.migdbclient.config.FxmlPath;
+import org.migdb.migdbclient.main.MainApp;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SetupNewDBConnectionController implements Initializable {
+
+	@FXML
+	private AnchorPane rootLayoutAnchorpane;
+	@FXML
+	private Label mysqlLabel;
+	@FXML
+	private Label mongoLabel;
+	
+	FXMLLoader loader = new FXMLLoader();
+
+	/**
+	 * Initialize method Called to initialize a controller after its root
+	 * element has been completely processed The location used to resolve
+	 * relative paths for the root object, or null if the location is not known
+	 * The resources used to localize the root object, or null if the root
+	 * object was not localized
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// MySQL label click event
+		mysqlLabel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent mouseevent) {
+				mysqlPopup();
+			}
+		});
+
+		// Mongo label click event
+		mongoLabel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent mouseevent) {
+				mongoPopup();
+			}
+		});
+	}
+
+	// Method for mysql window pop up
+	public void mysqlPopup() {
+		try {
+			loader.setLocation(MainApp.class.getResource(FxmlPath.MYSQLCONNECTION.getPath()));
+			AnchorPane mysql = loader.load();
+			rootLayoutAnchorpane.getChildren().clear();
+			rootLayoutAnchorpane.getChildren().add(mysql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Method for mongo window pop up
+	public void mongoPopup() {
+		try {
+			loader.setLocation(MainApp.class.getResource(FxmlPath.MONGOCONNECTION.getPath()));
+			AnchorPane mongo = loader.load();
+			rootLayoutAnchorpane.getChildren().clear();
+			rootLayoutAnchorpane.getChildren().add(mongo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
