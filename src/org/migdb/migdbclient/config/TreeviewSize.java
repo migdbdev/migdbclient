55b6056ffb40b9993f420df42cc5dc104790b2bb
NimeshaM <malkinimesha@gmail.com>
2016-06-10 12:42:50 +0530

    evaluation of table/column removal

--- src/org/migdb/migdbclient/config/TreeviewSize.java
+++ src/org/migdb/migdbclient/config/TreeviewSize.java
@@ -3,7 +3,11 @@ package org.migdb.migdbclient.config;
 public enum TreeviewSize {
 
 	TREEVIEWHEIGHT(200),
-	TREEVIEWIDTH(150);
+	TREEVIEWIDTH(150),
+	XSTART(10),
+	YSTART(10),
+	XSPACE(40),
+	YSPACE(20);
 	
 	private double size;
 	
