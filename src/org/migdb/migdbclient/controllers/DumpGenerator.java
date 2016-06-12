631561846ed4a9fd5233a310f417b5138f599ed6
Kanishka_Lahiru <lkanishka200@gmail.com>
2016-06-12 18:41:31 +0530

    File path
    Create enum for store file paths

---
+++ src/org/migdb/migdbclient/controllers/DumpGenerator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.migdb.migdbclient.controllers;
+
+/**
+ * @author Kani
+ *
+ */
+public class DumpGenerator {
+
+	public void generateDump() {
+		// Returns the runtime object associated with the current Java
+		// application
+		Runtime rt = Runtime.getRuntime();
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
