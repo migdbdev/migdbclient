be6036b0a32b78bf3237a9e79c41922056434e9b
lakshankasun <lkmadhusanka@gmail.com>
2016-06-23 09:53:57 +0530

    fixed fxmlPath

---
+++ src/org/migdb/migdbclient/config/FxmlPath.java
@@ -0,0 +1,24 @@
+package org.migdb.migdbclient.config;
+
+public enum FxmlPath {
+	
+	ROOTLAYOUT("/org/migdb/migdbclient/views/root/RootLayout.fxml"),
+	CONNECTIONMANAGER("/org/migdb/migdbclient/views/connectionmanager/ConnectionManager.fxml"),
+	MODIFICATIONEVALUATOR("/org/migdb/migdbclient/views/modificationevaluator/ModificationEvaluator.fxml"),
+	DATAMANAGER("/org/migdb/migdbclient/views/mongodatamanager/MongoDataManager.fxml"),
+	NEWDBCONNECTION("/org/migdb/migdbclient/views/connectionmanager/NewDBConnection.fxml"),
+	COLLECTIONMANAGER("/org/migdb/migdbclient/views/mongodatamanager/CollectionManager.fxml"),
+	DOCUMENTMANAGER("/org/migdb/migdbclient/views/mongodatamanager/DocumentManager.fxml"),
+	MANYTOMANYSELECTOR("/org/migdb/migdbclient/views/cardinalityselector/ManyToManySelector.fxml");
+	
+	private String path;
+	
+	FxmlPath(String path) {
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+}
