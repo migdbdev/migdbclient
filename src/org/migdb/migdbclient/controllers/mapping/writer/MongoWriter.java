4dac7f2e19104081554ff3aea0ddfa015bff85c7
lakshankasun <lkmadhusanka@gmail.com>
2016-07-20 11:20:36 +0530

    resolve DadabaseResource

--- src/org/migdb/migdbclient/controllers/mapping/writer/MongoWriter.java
+++ src/org/migdb/migdbclient/controllers/mapping/writer/MongoWriter.java
@@ -1,12 +1,40 @@
 package org.migdb.migdbclient.controllers.mapping.writer;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.migdb.migdbclient.config.FilePath;
 
 public class MongoWriter {
 
 	private JSONObject mappedJson;
+
 	
-	public void write (){
-		
+	
+	public MongoWriter() {
+		super();
+		JSONParser parser = new JSONParser();
+		try {
+			Object object = parser.parse(new FileReader(FilePath.DOCUMENT.getPath() + FilePath.COLLECTIONFILE.getPath()));
+			mappedJson = (JSONObject) object;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	}
+
+
+
+	public void write() {
+		System.out.println(mappedJson);
 	}
 }
