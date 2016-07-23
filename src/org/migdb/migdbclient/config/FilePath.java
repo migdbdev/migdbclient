a053ab5677b73e85c25d75bdd9a8816f78d65612
Kanishka_Lahiru <lkanishka200@gmail.com>
2016-07-23 23:57:42 +0530

    Enhance the file path separator
    Using File.separator instead of //

--- src/org/migdb/migdbclient/config/FilePath.java
+++ src/org/migdb/migdbclient/config/FilePath.java
@@ -3,17 +3,19 @@
  */
 package org.migdb.migdbclient.config;
 
+import java.io.File;
+
 /**
  * @author Kani
  *
  */
 public enum FilePath {
 
-	DOCUMENT(System.getProperty("user.home") + "\\" + "Documents\\MigDB"),
-	XMLPATH("\\Xmldump.xml"),
-	DBSTRUCTUREFILE("\\Database structure.json"),
-	DELETEDITEMFILE("\\Item deleted.json"),
-	COLLECTIONFILE("\\Collection.json");
+	DOCUMENT(System.getProperty("user.home") + File.separator + "Documents"+ File.separator + "MigDB" + File.separator),
+	XMLPATH("Xmldump.xml"),
+	DBSTRUCTUREFILE("Database structure.json"),
+	DELETEDITEMFILE("Item deleted.json"),
+	COLLECTIONFILE("Collection.json");
 
 	private String path;
 
