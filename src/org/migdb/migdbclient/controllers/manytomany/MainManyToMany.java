bb39cb42e85ad2a1690cf6a6bd40815530225298
lakshankasun <lkmadhusanka@gmail.com>
2016-11-06 18:47:31 +0530

    change existing referenced mapper

--- src/org/migdb/migdbclient/controllers/manytomany/MainManyToMany.java
+++ src/org/migdb/migdbclient/controllers/manytomany/MainManyToMany.java
@@ -1,8 +1,10 @@
 package org.migdb.migdbclient.controllers.manytomany;
 
 import org.json.simple.JSONObject;
+import org.migdb.migdbclient.controllers.mapping.changemapping.ChangeReferencing;
 import org.migdb.migdbclient.controllers.mapping.manytomany.ManyToMany;
 import org.migdb.migdbclient.controllers.mapping.writer.MongoWriter;
+import org.migdb.migdbclient.resources.ChangeStructure;
 import org.migdb.migdbclient.utils.ServiceAccessor;
 
 import com.google.gson.JsonObject;
@@ -15,10 +17,14 @@ public class MainManyToMany {
 //		ManyToMany many = new ManyToMany();
 //		many.identifyM2M();
 //		System.out.println("mapping module stopped...");
-		System.out.println("mapping module started...");
-		MongoWriter mongoWriter = new MongoWriter();
-		mongoWriter.write();
-		System.out.println("mapping module stopped...");
+//		System.out.println("mapping module started...");
+//		MongoWriter mongoWriter = new MongoWriter();
+//		mongoWriter.write();
+//		System.out.println("mapping module stopped...");
+		System.out.println("change module started...");
+		ChangeReferencing changeReferencing = new ChangeReferencing();
+		changeReferencing.change("employee","projects");
+		System.out.println("change module stopped...");
 		
 //		ServiceAccessor accessor = new ServiceAccessor();
 //		JSONObject jsonObject = accessor.getMappingModel();
