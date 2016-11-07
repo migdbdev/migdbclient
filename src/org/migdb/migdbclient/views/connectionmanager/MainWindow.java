e2b0136f35267cf85f5e03c985b28f6ca4895fff
Kanishka_Lahiru <lkanishka200@gmail.com>
2016-11-07 14:49:33 +0530

    Issue #9 and  #5 fixed
    Url needed to put issue #5

--- src/org/migdb/migdbclient/views/connectionmanager/MainWindow.java
+++ src/org/migdb/migdbclient/views/connectionmanager/MainWindow.java
@@ -3,9 +3,13 @@
  */
 package org.migdb.migdbclient.views.connectionmanager;
 
+import java.net.InetAddress;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.migdb.migdbclient.resources.threads.CheckInternetConnectivity;
+
+import javafx.application.Platform;
 import javafx.fxml.Initializable;
 
 /**
@@ -13,7 +17,7 @@ import javafx.fxml.Initializable;
  *
  */
 public class MainWindow implements Initializable {
-	
+
 	/**
 	 * Initialize method Called to initialize a controller after its root
 	 * element has been completely processed The location used to resolve
@@ -23,6 +27,37 @@ public class MainWindow implements Initializable {
 	 */
 	public void initialize(URL location, ResourceBundle resources) {
 		
+		/*CheckInternetConnectivity internetStatus = new CheckInternetConnectivity();
+
+		//Check internet connectivity in separate tread
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5000); // just emulates some loading time
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					// just updates the list view items at the
+					// Application Thread
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							while(true) {
+								try {
+									
+								} catch (Exception e2) {
+									// TODO: handle exception
+								}
+								System.out.println(internetStatus.testIneternet("google.com"));
+								
+							}
+						}
+					});
+				}
+			}
+		}).start();*/
+
 	}
 
 }
